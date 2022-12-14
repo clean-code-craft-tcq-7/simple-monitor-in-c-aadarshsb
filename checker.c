fd16603c7a4ce6c283f05ffeab61fc7bb5bf152f
aadarshsb <83934598+aadarshsb@users.noreply.github.com>
2022-12-14 14:13:54 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -86,7 +86,9 @@ void PrintTheError(char* ErrorString)
 }
 
 int main() {
+  printf("First one \n");
   batteryIsOk(25, 70, 0.7);
+  printf("Second one \n");
   batteryIsOk(50, 85, 0);
 //  assert(batteryIsOk(25, 70, 0.7));
 //  assert(!batteryIsOk(50, 85, 0));
